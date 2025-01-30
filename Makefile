5988efca15385e8b0388889dba08db1863d70a7a
FreddyMSchubert <github@frederickschubert.de>
2025-01-30 12:17:05 +0100

    Env Replacement Fixes, WordPress now properly works

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 COMPOSE := ./srcs/docker-compose.yml
 
+start: run
 run:
-	mkdir ./srcs/web
-	docker compose -f $(COMPOSE) up -d
+	mkdir -p ./srcs/web
+	docker compose -f $(COMPOSE) up --build
 stop:
 	docker compose -f $(COMPOSE)  down
--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -2,6 +2,7 @@
 
 set -e # Immediately exit if any command has a non-zero exit status.
 
-envsubst < /etc/mysql/init.sql > /tmp/init.sql
+envsubst < /etc/mysql/init.sql > /tmp/init_substituted.sql
+mv /tmp/init_substituted.sql /etc/mysql/init.sql
 
 exec mysqld_safe
