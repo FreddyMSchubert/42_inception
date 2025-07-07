55bedb995a9ea078fe9aa51e535b1dd43eeaa279
FreddyMSchubert <github@frederickschubert.de>
2025-07-07 16:54:35 +0200

    http to https redirect, minor fixes

--- srcs/requirements/nginx/envsubstitution.sh
+++ srcs/requirements/nginx/envsubstitution.sh
@@ -2,7 +2,7 @@
 
 set -e # Immediately exit if any command has a non-zero exit status.
 
-envsubst < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default1
+envsubst '$DOMAIN_NAME' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default1
 mv /etc/nginx/sites-available/default1 /etc/nginx/sites-available/default
 
 exec nginx -g 'daemon off;'
