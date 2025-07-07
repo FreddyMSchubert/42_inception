825d75041359d4b0f0eb6a2733a23c64d3670fc4
FreddyMSchubert <github@frederickschubert.de>
2025-07-07 17:00:08 +0200

    Skip wp setup if wp config already present

--- srcs/requirements/wordpress/install-wp.sh
+++ srcs/requirements/wordpress/install-wp.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-
 set -e
 
-chown -R www-data:www-data /var/www/html
-
 cd /var/www/html
-# download wordpress command line interface to download & manage wordpress
-if [ ! -f wp-cli.phar ]; then
+
+# Only download & configure to download & manage wordpress if WP isn’t already set up
+if [ ! -f wp-config.php ]; then
+	echo "[wp-setup] Fresh install: downloading core, generating config, installing..."
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
-fi
 
-if [ ! -f wp-load.php ]; then
 	./wp-cli.phar core download --allow-root
+	./wp-cli.phar config create \
+		--dbname=${MARIA_DB_DATABASE_NAME} \
+		--dbuser=${MARIA_DB_ROOT_USER} \
+		--dbpass=${MARIA_DB_ROOT_PASSWORD} \
+		--dbhost=mariadb \
+		--allow-root
+
+	./wp-cli.phar core install \
+		--url=https://${DOMAIN_NAME} \
+		--title=inception \
+		--admin_user=${WORDPRESS_ADMIN_USER} \
+		--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+		--admin_email=${WORDPRESS_ADMIN_EMAIL} \
+		--allow-root
+
+	./wp-cli.phar user create "${WORDPRESS_TEST_USER}" "${WORDPRESS_TEST_USER_EMAIL}" \
+		--role=author \
+		--user_pass="${WORDPRESS_TEST_USER_PASSWORD}" \
+		--allow-root
+else
+	echo "[wp-setup] Detected existing installation – skipping setup."
 fi
 
-./wp-cli.phar config create \
-			--dbname=${MARIA_DB_DATABASE_NAME} \
-			--dbuser=${MARIA_DB_ROOT_USER} \
-			--dbpass=${MARIA_DB_ROOT_PASSWORD} \
-			--dbhost=mariadb \
-			--allow-root
-./wp-cli.phar core install \
-			--url=https://${DOMAIN_NAME} \
-			--title=inception \
-			--admin_user=${WORDPRESS_ADMIN_USER} \
-			--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
-			--admin_email=${WORDPRESS_ADMIN_EMAIL} \
-			--allow-root
-./wp-cli.phar user create "${WORDPRESS_TEST_USER}" "${WORDPRESS_TEST_USER_EMAIL}" \
-			--role=author \
-			--user_pass="${WORDPRESS_TEST_USER_PASSWORD}" \
-			--allow-root
-
+# Finally, always start PHP-FPM
 php-fpm7.4 -F
