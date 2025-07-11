81d011a5228fb3cfb04f548a419535bef1f63468
FreddyMSchubert <github@frederickschubert.de>
2025-07-11 18:22:36 +0200

    add nginx wait for finished wordpress downlaod infinite loop

--- srcs/requirements/nginx/envsubstitution.sh
+++ srcs/requirements/nginx/envsubstitution.sh
@@ -2,6 +2,11 @@
 
 set -e # Immediately exit if any command has a non-zero exit status.
 
+while [ ! -f /var/www/html/index.php ]; do
+    echo "[nginx] waiting for WordPress to finish installing…" >&2
+    sleep 2
+done
+
 envsubst '$DOMAIN_NAME' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default1
 mv /etc/nginx/sites-available/default1 /etc/nginx/sites-available/default
 
