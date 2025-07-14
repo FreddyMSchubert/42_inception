bcc5f8e813713710b2b7a85ca3eaba019a6327ce
FreddyMSchubert <github@frederickschubert.de>
2025-07-14 17:37:06 +0200

    make built-in rot@localhost user also require root password in mariadb

--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE IF NOT EXISTS ${MARIA_DB_DATABASE_NAME};
 CREATE USER '${MARIA_DB_ROOT_USER}'@'%' IDENTIFIED BY '${MARIA_DB_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MARIA_DB_DATABASE_NAME}.* TO '${MARIA_DB_ROOT_USER}'@'%' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MARIA_DB_ROOT_PASSWORD}');
 FLUSH PRIVILEGES;
 
 -- % is used as a wildcard so ${MARIA_DB_ROOT_USER} can connect from any host
 -- wordpress.* means all tables in the wordpress database
+-- ALTER USER locks built-in root@localhost with root password
 -- FLUSH PRIVILEGES reloads the grant tables in memory to apply the changes
